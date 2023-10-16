94dae5101112fab6e48c4c407dec37d1f146d846
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2023-10-16 21:30:47 +0200

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Alejandro Sierro Galán on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
