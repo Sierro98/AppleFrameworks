2543be080a0af6ce445275493d3e3caac1da9392
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2023-10-21 21:24:04 +0200

    Starting the theme selector settings

--- AppleFrameworks/Screens/GeneralSettingsView/GeneralSettingsView.swift
+++ AppleFrameworks/Screens/GeneralSettingsView/GeneralSettingsView.swift
@@ -9,14 +9,27 @@ import SwiftUI
 
 struct GeneralSettingsView: View {
     
+    @State var toggleList: Array<Bool> = [true, false, false]
+    
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Display"),
                         footer: Text("Select if you prefer a list style for the frameworks list.")) {
-                    Toggle(isOn: .constant(false), label: {
+                    Toggle(isOn: .constant(false)) {
                         Text("List Style")
-                    })
+                    }
+                }
+                Section(header: Text("Theme")) {
+                    Toggle(isOn: $toggleList[0]) {
+                        Text("Light Theme")
+                    }
+                    Toggle(isOn: $toggleList[1]) {
+                        Text("Dark Theme")
+                    }
+                    Toggle(isOn: $toggleList[2]) {
+                        Text("System Theme")
+                    }
                 }
                 Section {
                     Link(destination: URL(string: "https://www.linkedin.com/in/alejandro-sierro-galán-15016b261/")!) {
